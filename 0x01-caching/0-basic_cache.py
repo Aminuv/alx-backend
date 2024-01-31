9c32f5fa4de878acb942d1180a88bf32f8482ddf
Aminuv <mohamed.elmarbouh07@gmail.com>
2024-01-31 18:13:49 +0000

    test

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" The class 'BasicCache' inherits """
+""" Class BasicCache inherits from BaseCaching """
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ The class """
+    """ class BasicCache """
 
     def put(self, key, item):
-        """ Function """
+        """ function put """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ get """
+        """ function get """
         if key in self.cache_data:
             return self.cache_data[key]
         return None
